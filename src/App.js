75a02f43b4fcd7da032f97e33202650989ecb2a5
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-27 09:00:03 -0300

    Criação das rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/about" component={ About } />
+        <Route exact path="/skills" component={ Skills } />
+        <Route exact path="/projects" component={ Projects } />
+        <Route exact path="/contact" component={ Contact } />
+      </Switch>
     </div>
   );
 }
