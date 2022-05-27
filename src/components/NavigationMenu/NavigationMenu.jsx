235dd7524ccd35deb58edeaa9cecfd417ee0e1b2
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-27 11:10:32 -0300

    Implementação do menu Hamburguer

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import './NavigationMenu.css';
 
 export default class NavigationMenu extends Component{
+  state = {
+    isMenuOpen: false,
+  }
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({
+      isMenuOpen: !prevState.isMenuOpen,
+    }))
+  }
+  
   render() {
+    const { isMenuOpen } = this.state;
     return (
-      <nav className="navigation">
-        <ul className="links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/skills">Skills</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
+      <section className={ isMenuOpen ? "icon iconActive" : "icon"} onClick={ this.toggleMenu}>
+          <div className="hamburguer"></div>
+          <div className={ isMenuOpen ? "menu menu-open" : "menu menu-closed" }>
+            {isMenuOpen && (
+              <nav className="links">
+                <ul>
+                  <li><Link to="/" className="link">Home</Link></li>
+                  <li><Link to="/about" className="link">About</Link></li>
+                  <li><Link to="/skills" className="link">Skills</Link></li>
+                  <li><Link to="/projects" className="link">Projects</Link></li>
+                  <li><Link to="/contact" className="link">Contact</Link></li>
+                </ul>
+              </nav>
+            )}
+          </div>
+      </section>
     );
   }
 }
--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import NavigationMenu from '../../NavigationMenu/NavigationMenu';
 
 export default class Header extends Component {
   render () {
@@ -8,13 +9,7 @@ export default class Header extends Component {
         <section className="Logo">
           BR - DevWeb
         </section>
-        <nav className="navigation">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/skills">Skills</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/contact">Contact</Link>
-        </nav>
+        <NavigationMenu />
       </header>
     );
   }
