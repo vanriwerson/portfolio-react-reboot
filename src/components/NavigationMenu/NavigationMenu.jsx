e1d4822a9c77597802c4d6741f185fe7ed2c75b1
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-27 14:40:49 -0300

    Ajuste no toggle button

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -16,22 +16,24 @@ export default class NavigationMenu extends Component{
   render() {
     const { isMenuOpen } = this.state;
     return (
-      <section className={ isMenuOpen ? "icon iconActive" : "icon"} onClick={ this.toggleMenu}>
-          <div className="hamburguer"></div>
-          
-          <div className={ isMenuOpen ? "menu menu-open" : "menu menu-closed" }>
-            {isMenuOpen && (
-              <nav className="links">
-                <ul>
-                  <li><Link to="/" className="link">Home</Link></li>
-                  <li><Link to="/about" className="link">About</Link></li>
-                  <li><Link to="/skills" className="link">Skills</Link></li>
-                  <li><Link to="/projects" className="link">Projects</Link></li>
-                  <li><Link to="/contact" className="link">Contact</Link></li>
-                </ul>
-              </nav>
-            )}
-          </div>
+      <section className="toggle-container">
+        <div
+          className={isMenuOpen ? "toggle-button active" : "toggle-button"}
+          onClick={ this.toggleMenu }
+        >
+          <div className="line"></div>
+        </div>
+        {isMenuOpen && (
+          <nav className="links">
+            <ul>
+              <li><Link to="/" className="link">Home</Link></li>
+              <li><Link to="/about" className="link">About</Link></li>
+              <li><Link to="/skills" className="link">Skills</Link></li>
+              <li><Link to="/projects" className="link">Projects</Link></li>
+              <li><Link to="/contact" className="link">Contact</Link></li>
+            </ul>
+          </nav>
+        )}
       </section>
     );
   }
