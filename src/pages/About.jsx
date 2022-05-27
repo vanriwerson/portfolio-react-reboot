0feba94fc43617ff68ed9927e932a9f8a1211a8f
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-27 08:51:13 -0300

    Estrutura inicial das páginas

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Header from "../components/shared/Header/Header";
+
+export default class About extends Component{
+  render () {
+    return(
+      <section>
+        <Header />
+      </section>
+    );
+  }
+}
