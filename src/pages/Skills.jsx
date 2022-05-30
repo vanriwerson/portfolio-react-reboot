abf7c73f03627a0deb6fa77831c29b5a409a7794
Bruno Riwerson Silva <riwerson@gmail.com>
2022-05-30 14:09:34 -0300

    Ajustes na navegação

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import HardSkillCard from "../components/HardSkillCard/HardSkillCard";
 import Footer from "../components/shared/Footer/Footer";
 import Header from "../components/shared/Header/Header";
+import './Skills.css'
 
 export default class Skills extends Component{
   render () {
     return(
-      <section>
+      <section className="skills">
         <Header />
         <HardSkillCard />
         <Footer />
