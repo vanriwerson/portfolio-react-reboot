8fb0ab33d5515c047f628a65b78aed92b8ee744f
Bruno Riwerson Silva <riwerson@gmail.com>
2022-07-06 11:26:19 -0300

    Algum estilo em Home e Skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Footer from "../components/shared/Footer/Footer";
 import Header from "../components/shared/Header/Header";
+import profile from '../images/profile.png'
+import './Home.css';
 
 export default class Home extends Component{
   render () {
     return(
-      <section>
+      <section className="home">
         <Header />
-        <Footer />
+        <main className="content">
+          <h1>Boas-vindas ao meu Portfólio</h1>
+          <img src={ profile } alt="Foto de Bruno Riwerson" />
+        </main>
+        <Footer className="footer" />
       </section>
     );
   }
