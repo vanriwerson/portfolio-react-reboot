fd2ddde36014f7ee9a77717e6d6e4859ec6dc907
Bruno Riwerson Silva <riwerson@gmail.com>
2022-07-25 08:24:44 -0300

    Ajustes na landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,11 @@ export default class Home extends Component{
         <main className="content">
           <h1>Boas-vindas ao meu Portfólio</h1>
           <img src={ profile } alt="Foto de Bruno Riwerson" />
+
+          <div className="cta-wrapper">
+            <p>Meu nome é Bruno, Desenvolvedor Web aficionado pela geração de soluções práticas e eficazes. Fico muito feliz por seu interesse em conhecer um pouco do meu trabalho.</p>
+            <button>Como posso te ajudar?</button>
+          </div>
         </main>
         <Footer className="footer" />
       </section>
