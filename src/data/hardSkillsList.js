d19f48d4175a1d845ae6057a315782322c891379
Bruno Riwerson Silva <riwerson@gmail.com>
2022-07-26 15:49:40 -0300

    Adição de skills

--- src/data/hardSkillsList.js
+++ src/data/hardSkillsList.js
@@ -1,7 +1,9 @@
 import html from '../images/skills/html.svg';
 import css from '../images/skills/css.svg';
 import javascript from '../images/skills/javascript.svg';
+import jest from '../images/skills/jest.png'
 import react from '../images/skills/react.svg';
+import testingLibrary from '../images/skills/testing-library.png';
 import vuejs from '../images/skills/vuejs.svg';
 import typescript from '../images/skills/typescript.svg';
 import java from '../images/skills/java.svg';
@@ -32,41 +34,55 @@ const hardSkillsList = [
   },
   {
     id: 'tech04',
+    imgUrl: jest,
+    techName: 'Jest',
+    techDescription: 'Jest é um framework de teste unitário de código aberto em JavaScript criado pelo time do Facebook a partir do framework Jasmine.',
+    moreAbout: 'https://jestjs.io/pt-BR/docs/getting-started',
+  },
+  {
+    id: 'tech05',
     imgUrl: react,
     techName: 'React JS',
     techDescription: 'React JS é uma biblioteca JavaScript criada pelo time do Facebook e voltada para a implementação de interfaces de usuário (UI - User Interface)',
     moreAbout: 'https://pt-br.reactjs.org/docs/getting-started.html',
   },
   {
-    id: 'tech05',
+    id: 'tech06',
+    imgUrl: testingLibrary,
+    techName: 'Testing Library',
+    techDescription: 'É um conjunto de utilitários que permitem testar componentes React sem depender dos detalhes de implementação.',
+    moreAbout: 'https://testing-library.com/docs/react-testing-library/intro/',
+  },
+  {
+    id: 'tech07',
     imgUrl: vuejs,
     techName: 'Vue JS with VuetiFy',
     techDescription: `Vue (lê-se "viu") é um framework progressivo para a construção de UI e muito utilizado na criação SPA's (Single-Page Applications)`,
     moreAbout: 'https://br.vuejs.org/v2/guide/index.html',
   },
   {
-    id: 'tech06',
+    id: 'tech08',
     imgUrl: typescript,
     techName: 'Typescript',
     techDescription: 'O Typescript é um superconjunto de ferramentas e recursos não nativos da linguagem  JavaScript.',
     moreAbout: 'https://www.typescriptlang.org/docs/',
   },
   {
-    id: 'tech07',
+    id: 'tech09',
     imgUrl: java,
     techName: 'JAVA',
     techDescription: 'Java é uma linguagem de  orientada a objetos e ambiente computacional multiplataforma.',
     moreAbout: 'https://docs.oracle.com/en/java/',
   },
   {
-    id: 'tech08',
+    id: 'tech10',
     imgUrl: git,
     techName: 'Git',
     techDescription: 'GIT é um Sistema de Controle de Versão Distribuído que guarda todo o histórico de alterações do código gerenciado.',
     moreAbout: 'https://git-scm.com/docs/git/pt_BR',
   },
   {
-    id: 'tech09',
+    id: 'tech11',
     imgUrl: linux,
     techName: 'Linux',
     techDescription: 'O Linux é um sistema operacional open source baseado em Unix criado para desktops, mas que também é usado em servidores, smartphones, tablets e outros tipos de dispositivos.',
