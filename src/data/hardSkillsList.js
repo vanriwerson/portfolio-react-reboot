cfb6f8e9b153407e2c67cddcaa97cc18bdf89769
Bruno Riwerson Silva <riwerson@gmail.com>
2022-08-18 16:04:33 -0300

    Adding techs

--- src/data/hardSkillsList.js
+++ src/data/hardSkillsList.js
@@ -1,92 +1,119 @@
-import html from '../images/skills/html.svg';
-import css from '../images/skills/css.svg';
-import javascript from '../images/skills/javascript.svg';
-import jest from '../images/skills/jest.png'
-import react from '../images/skills/react.svg';
-import testingLibrary from '../images/skills/testing-library.png';
-import vuejs from '../images/skills/vuejs.svg';
-import typescript from '../images/skills/typescript.svg';
-import java from '../images/skills/java.svg';
-import git from '../images/skills/git.svg';
-import linux from '../images/skills/linux.svg';
+import html from "../images/skills/html.svg";
+import css from "../images/skills/css.svg";
+import javascript from "../images/skills/javascript.svg";
+import jest from "../images/skills/jest.png";
+import react from "../images/skills/react.svg";
+import testingLibrary from "../images/skills/testing-library.png";
+import redux from "../images/skills/redux.svg";
+import vuejs from "../images/skills/vuejs.svg";
+import typescript from "../images/skills/typescript.svg";
+import java from "../images/skills/java.svg";
+import git from "../images/skills/git.svg";
+import linux from "../images/skills/linux.svg";
+import mysql from "../images/skills/mysql.png";
 
 const hardSkillsList = [
   {
-    id: 'tech01',
+    id: "tech01",
     imgUrl: html,
-    techName: 'HTML',
-    techDescription: 'Do inglês HiperText Markup Language (Linguagem de Marcação de Hipertexto), é a ferramenta básica para estruturar conteúdo em um website.',
-    moreAbout: 'https://developer.mozilla.org/pt-BR/docs/Web/HTML',
+    techName: "HTML",
+    techDescription:
+      "Do inglês HiperText Markup Language (Linguagem de Marcação de Hipertexto), é a ferramenta básica para estruturar conteúdo em um website.",
+    moreAbout: "https://developer.mozilla.org/pt-BR/docs/Web/HTML",
   },
   {
-    id: 'tech02',
+    id: "tech02",
     imgUrl: css,
-    techName: 'CSS',
-    techDescription: 'O CSS (Cascading Style Sheet) é utilizado para adicionar estilo ao HTML. Toda a estética de um site vem daqui.',
-    moreAbout: 'https://developer.mozilla.org/pt-BR/docs/Web/CSS',
+    techName: "CSS",
+    techDescription:
+      "O CSS (Cascading Style Sheet) é utilizado para adicionar estilo ao HTML. Toda a estética de um site vem daqui.",
+    moreAbout: "https://developer.mozilla.org/pt-BR/docs/Web/CSS",
   },
   {
-    id: 'tech03',
+    id: "tech03",
     imgUrl: javascript,
-    techName: 'Javascript',
-    techDescription: 'É uma linguagem de programação através da qual podemos criar conteúdo dinâmico e/ou interações com o usuário.',
-    moreAbout: 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript',
+    techName: "Javascript",
+    techDescription:
+      "É uma linguagem de programação através da qual podemos criar conteúdo dinâmico e/ou interações com o usuário.",
+    moreAbout: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
   },
   {
-    id: 'tech04',
+    id: "tech04",
     imgUrl: jest,
-    techName: 'Jest',
-    techDescription: 'Jest é um framework de teste unitário de código aberto em JavaScript criado pelo time do Facebook a partir do framework Jasmine.',
-    moreAbout: 'https://jestjs.io/pt-BR/docs/getting-started',
+    techName: "Jest",
+    techDescription:
+      "Jest é um framework de teste unitário de código aberto em JavaScript criado pelo time do Facebook a partir do framework Jasmine.",
+    moreAbout: "https://jestjs.io/pt-BR/docs/getting-started",
   },
   {
-    id: 'tech05',
+    id: "tech05",
     imgUrl: react,
-    techName: 'React JS',
-    techDescription: 'React JS é uma biblioteca JavaScript criada pelo time do Facebook e voltada para a implementação de interfaces de usuário (UI - User Interface)',
-    moreAbout: 'https://pt-br.reactjs.org/docs/getting-started.html',
+    techName: "React JS",
+    techDescription:
+      "React JS é uma biblioteca JavaScript criada pelo time do Facebook e voltada para a implementação de interfaces de usuário (UI - User Interface)",
+    moreAbout: "https://pt-br.reactjs.org/docs/getting-started.html",
   },
   {
-    id: 'tech06',
+    id: "tech06",
     imgUrl: testingLibrary,
-    techName: 'Testing Library',
-    techDescription: 'É um conjunto de utilitários que permitem testar componentes React sem depender dos detalhes de implementação.',
-    moreAbout: 'https://testing-library.com/docs/react-testing-library/intro/',
+    techName: "Testing Library",
+    techDescription:
+      "É um conjunto de utilitários que permitem testar componentes React sem depender dos detalhes de implementação.",
+    moreAbout: "https://testing-library.com/docs/react-testing-library/intro/",
   },
   {
-    id: 'tech07',
+    id: "tech07",
+    imgUrl: redux,
+    techName: "Redux",
+    techDescription: `É uma biblioteca criada para gerenciar estados em uma aplicação de forma global.`,
+    moreAbout: "https://react-redux.js.org/introduction/getting-started",
+  },
+  {
+    id: "tech08",
     imgUrl: vuejs,
-    techName: 'Vue JS with VuetiFy',
+    techName: "Vue JS with VuetiFy",
     techDescription: `Vue (lê-se "viu") é um framework progressivo para a construção de UI e muito utilizado na criação SPA's (Single-Page Applications)`,
-    moreAbout: 'https://br.vuejs.org/v2/guide/index.html',
+    moreAbout: "https://br.vuejs.org/v2/guide/index.html",
   },
   {
-    id: 'tech08',
+    id: "tech09",
     imgUrl: typescript,
-    techName: 'Typescript',
-    techDescription: 'O Typescript é um superconjunto de ferramentas e recursos não nativos da linguagem  JavaScript.',
-    moreAbout: 'https://www.typescriptlang.org/docs/',
+    techName: "Typescript",
+    techDescription:
+      "O Typescript é um superconjunto de ferramentas e recursos não nativos da linguagem  JavaScript.",
+    moreAbout: "https://www.typescriptlang.org/docs/",
   },
   {
-    id: 'tech09',
+    id: "tech10",
     imgUrl: java,
-    techName: 'JAVA',
-    techDescription: 'Java é uma linguagem de programação orientada a objetos e ambiente computacional multiplataforma.',
-    moreAbout: 'https://docs.oracle.com/en/java/',
+    techName: "JAVA",
+    techDescription:
+      "Java é uma linguagem de programação orientada a objetos e ambiente computacional multiplataforma.",
+    moreAbout: "https://docs.oracle.com/en/java/",
   },
   {
-    id: 'tech10',
+    id: "tech11",
     imgUrl: git,
-    techName: 'Git',
-    techDescription: 'GIT é um Sistema de Controle de Versão Distribuído que guarda todo o histórico de alterações do código gerenciado.',
-    moreAbout: 'https://git-scm.com/docs/git/pt_BR',
+    techName: "Git",
+    techDescription:
+      "GIT é um Sistema de Controle de Versão Distribuído que guarda todo o histórico de alterações do código gerenciado.",
+    moreAbout: "https://git-scm.com/docs/git/pt_BR",
   },
   {
-    id: 'tech11',
+    id: "tech12",
     imgUrl: linux,
-    techName: 'Linux',
-    techDescription: 'O Linux é um sistema operacional open source baseado em Unix criado para desktops, mas que também é usado em servidores, smartphones, tablets e outros tipos de dispositivos.',
-    moreAbout: '',
+    techName: "Linux",
+    techDescription:
+      "O Linux é um sistema operacional open source baseado em Unix criado para desktops, mas que também é usado em servidores, smartphones, tablets e outros tipos de dispositivos.",
+    moreAbout: "",
+  },
+  {
+    id: "tech14",
+    imgUrl: mysql,
+    techName: "MySQL",
+    techDescription:
+      "É um sistema open-source de gerenciamento de base de dados relacional que utiliza a liguagem SQL (Structured Query Language).",
+    moreAbout: "https://dev.mysql.com/doc/",
   },
 ];
 
